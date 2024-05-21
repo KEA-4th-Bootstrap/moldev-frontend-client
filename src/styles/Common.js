0fc0f8e76346238ff9b3d9ff57189cc8137fa48b
Ginieee <kaj1226@naver.com>
2024-05-21 21:10:57 +0900

    ✨ [Feat] twin.marco styling에 사용하기 위한 Common Styling 파일 생성

---
+++ src/styles/Common.js
@@ -0,0 +1,38 @@
+export const Common = {
+  color: {
+    black: '#000000',
+    white: '#FFFFFF',
+    transparent: 'transparent',
+    main: '#00A9FF',
+    info: '#DCF1FB',
+    disable: '#B2E5FF',
+    sub: {
+      300: '#89CFF3',
+      200: '#A0E9FF',
+      100: '#CDF5FD',
+      50: '#F4F8FC',
+    },
+    dark: {
+      300: '#1A1A1A',
+      200: '#2A2A2A',
+      100: '#3A3A3A',
+      50: '#4A4A4A',
+    },
+    gray: {
+      900: '#424242',
+      800: '#565656',
+      700: '#6F6F6F',
+      600: '#8E8E8E',
+      500: '#9C9C9C',
+      400: '#B0B0B0',
+      300: '#BDBDBD',
+      200: '#D1D1D1',
+      100: '#E0E0E0',
+      50: '#F2F2F2',
+    },
+    negative: '#FF5151',
+    positive: '#2AE45E',
+    warning: '#FFCE4F',
+    message: '#80BAEF',
+  },
+};
