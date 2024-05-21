b4ae6e5fb7a9395d32ae5c24f61f53be1495d896
Ginieee <kaj1226@naver.com>
2024-05-21 21:12:02 +0900

    ✨ [Feat] 타입 활용을 위한 dummy.ts 생성

---
+++ src/data/dummy.ts
@@ -0,0 +1,531 @@
+import {
+  commentType,
+  islandListItemType,
+  postListItemType,
+  recentListItemType,
+} from './type';
+
+export const dummyProject: postListItemType[] = [
+  {
+    moldevId: '1',
+    id: 1,
+    title: '프로젝트1',
+    content:
+      '프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 프로젝트1 내용 ',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'project',
+    img: 'https://media.bunjang.co.kr/product/178099314_1_1644048789_w360.jpg',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 2,
+    title: '프로젝트2',
+    content: '프로젝트2 내용',
+    createdAt: '2021-08-02',
+    visit: 200,
+    category: 'project',
+    img: 'https://d2gfz7wkiigkmv.cloudfront.net/pickin/2/1/2/pX6kgTynTN6n2XD0tqaLSQ',
+    userImg: 'https://via.placeholder.com/150',
+    userName: '김철수',
+  },
+  {
+    moldevId: '1',
+    id: 3,
+    title: '프로젝트3',
+    content: '프로젝트3 내용',
+    createdAt: '2021-08-03',
+    visit: 300,
+    category: 'project',
+    img: 'https://pbs.twimg.com/media/FHILO2jaMAYi9Bz?format=jpg&name=large',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 2,
+    title: '프로젝트2',
+    content: '프로젝트2 내용',
+    createdAt: '2021-08-02',
+    visit: 200,
+    category: 'project',
+    img: 'https://d2gfz7wkiigkmv.cloudfront.net/pickin/2/1/2/pX6kgTynTN6n2XD0tqaLSQ',
+    userImg: 'https://via.placeholder.com/150',
+    userName: '김철수',
+  },
+  {
+    moldevId: '1',
+    id: 3,
+    title: '프로젝트3',
+    content: '프로젝트3 내용',
+    createdAt: '2021-08-03',
+    visit: 300,
+    category: 'project',
+    img: 'https://pbs.twimg.com/media/FHILO2jaMAYi9Bz?format=jpg&name=large',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+];
+export const dummyActivity: postListItemType[] = [
+  {
+    moldevId: '1',
+    id: 1,
+    title: '대외활동1',
+    content: '대외활동1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'activity',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '대외활동1',
+    content: '대외활동1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'activity',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '대외활동1',
+    content: '대외활동1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'activity',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '대외활동1',
+    content: '대외활동1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'activity',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '대외활동1',
+    content: '대외활동1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'activity',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+];
+
+export const dummyAwards: postListItemType[] = [
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '수상이력1',
+    content: '수상이력1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'awards',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+];
+
+export const dummyTrouble: postListItemType[] = [
+  {
+    moldevId: '1',
+    id: 1,
+    title: '트러블슈팅1',
+    content: '트러블슈팅1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'trouble',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '트러블슈팅1',
+    content: '트러블슈팅1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'trouble',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '트러블슈팅1',
+    content: '트러블슈팅1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'trouble',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '트러블슈팅1',
+    content: '트러블슈팅1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'trouble',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+  {
+    moldevId: '1',
+    id: 1,
+    title: '트러블슈팅1',
+    content: '트러블슈팅1 내용',
+    createdAt: '2021-08-01',
+    visit: 100,
+    category: 'trouble',
+    img: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    userImg: 'https://via.placeholder.com/150',
+  },
+];
+
+export const dummyIslandList: islandListItemType[] = [
+  {
+    moldevId: '1',
+    userName: '주원이',
+    userImg: 'https://via.placeholder.com/150',
+    islandName: '워니',
+    visit: 100,
+    updated: '2021-08-01',
+    articles: [
+      {
+        id: 1,
+        title: '프로젝트1',
+        createdAt: '2021-08-01',
+      },
+      {
+        id: 2,
+        title: '프로젝트2',
+        createdAt: '2021-08-02',
+      },
+      {
+        id: 3,
+        title: '프로젝트3',
+        createdAt: '2021-08-03',
+      },
+    ],
+  },
+  {
+    moldevId: '1',
+    userName: '강어진',
+    userImg: 'https://via.placeholder.com/150',
+    islandName: '어진의',
+    visit: 100,
+    updated: '2021-08-01',
+    articles: [
+      {
+        id: 1,
+        title: '프로젝트1',
+        createdAt: '2021-08-01',
+      },
+      {
+        id: 2,
+        title: '프로젝트2',
+        createdAt: '2021-08-02',
+      },
+      {
+        id: 3,
+        title: '프로젝트3',
+        createdAt: '2021-08-03',
+      },
+    ],
+  },
+  {
+    moldevId: '1',
+    userName: '김어진',
+    userImg: 'https://via.placeholder.com/150',
+    islandName: '어진의',
+    visit: 100,
+    updated: '2021-08-01',
+    articles: [
+      {
+        id: 1,
+        title: '프로젝트1',
+        createdAt: '2021-08-01',
+      },
+      {
+        id: 2,
+        title: '프로젝트2',
+        createdAt: '2021-08-02',
+      },
+      {
+        id: 3,
+        title: '프로젝트3',
+        createdAt: '2021-08-03',
+      },
+    ],
+  },
+  {
+    moldevId: '1',
+    userName: '어진강',
+    userImg: 'https://via.placeholder.com/150',
+    islandName: '진강의',
+    visit: 100,
+    updated: '2021-08-01',
+    articles: [
+      {
+        id: 1,
+        title: '프로젝트1',
+        createdAt: '2021-08-01',
+      },
+      {
+        id: 2,
+        title: '프로젝트2',
+        createdAt: '2021-08-02',
+      },
+      {
+        id: 3,
+        title: '프로젝트3',
+        createdAt: '2021-08-03',
+      },
+    ],
+  },
+  {
+    moldevId: '1',
+    userName: '지니어',
+    userImg: 'https://via.placeholder.com/150',
+    islandName: '니어의',
+    visit: 100,
+    updated: '2021-08-01',
+    articles: [
+      {
+        id: 1,
+        title: '프로젝트1',
+        createdAt: '2021-08-01',
+      },
+      {
+        id: 2,
+        title: '프로젝트2',
+        createdAt: '2021-08-02',
+      },
+      {
+        id: 3,
+        title: '프로젝트3',
+        createdAt: '2021-08-03',
+      },
+    ],
+  },
+];
+
+export const dummyPost: postListItemType = {
+  moldevId: '1',
+  id: 1,
+  title: '프로젝트1',
+  content: `
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+    <div>안녕하세요</div>
+    <div>반가워요</div>
+    <pre>코드블록이에요</pre>
+    <h1>제목1</h1>
+    <h2>제목2</h2>
+    <h3>제목3</h3>
+  `,
+  createdAt: '2021-08-01',
+  visit: 100,
+  category: 'activity',
+  img: 'https://via.placeholder.com/150',
+  userName: '복복복',
+  userImg: 'https://via.placeholder.com/150',
+};
+
+export const dummyRecentList: recentListItemType[] = [
+  {
+    postId: 1,
+    title: '프로젝트1',
+    createdAt: '2021-08-01',
+    isActive: true,
+  },
+  {
+    postId: 1,
+    title: '프로젝트1',
+    createdAt: '2021-08-01',
+    isActive: false,
+  },
+  {
+    postId: 1,
+    title: '프로젝트1',
+    createdAt: '2021-08-01',
+    isActive: false,
+  },
+  {
+    postId: 1,
+    title: '프로젝트1',
+    createdAt: '2021-08-01',
+    isActive: false,
+  },
+  {
+    postId: 1,
+    title: '프로젝트1',
+    createdAt: '2021-08-01',
+    isActive: false,
+  },
+];
+
+export const dummyCommetList: commentType[] = [
+  {
+    commentId: 1,
+    content: '댓글1',
+    createdAt: '2021-08-01',
+    userImg: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    islandName: '철수의',
+    reply: [
+      {
+        commentId: 2,
+        content: '대댓글1',
+        createdAt: '2021-08-01',
+        userImg: 'https://via.placeholder.com/150',
+        userName: '김철수',
+        islandName: '철수의',
+        reply: [],
+      },
+    ],
+  },
+  {
+    commentId: 1,
+    content: '댓글1',
+    createdAt: '2021-08-01',
+    userImg: 'https://via.placeholder.com/150',
+    userName: '김철수',
+    islandName: '철수의',
+    reply: [
+      {
+        commentId: 2,
+        content: '대댓글1',
+        createdAt: '2021-08-01',
+        userImg: 'https://via.placeholder.com/150',
+        userName: '김철수',
+        islandName: '철수의',
+        reply: [],
+      },
+      {
+        commentId: 2,
+        content: '대댓글1',
+        createdAt: '2021-08-01',
+        userImg: 'https://via.placeholder.com/150',
+        userName: '김철수',
+        islandName: '철수의',
+        reply: [],
+      },
+    ],
+  },
+];
