1b1aea0a2480523b1624c3e16b51f5a2d93e5181
Ginieee <kaj1226@naver.com>
2024-05-21 21:38:44 +0900

    ✨ [Feat] 검색 페이지 : 게시글 및 섬 검색결과 스켈레톤 생성

---
+++ src/components/sidebar/search/SearchResultPostSkeleton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchResultPostSkeleton = () => {
+  return (
+    <div className="w-full flex flex-col rounded-block bg-white">
+      <div className="w-full flex items-start justify-between p-8 gap-x-12">
+        <div className="grow h-[98px] flex flex-col gap-y-6 items-start justify-center">
+          <div className="w-full h-[20px] rounded-block bg-gray-100" />
+          <div className="w-full grow rounded-block bg-gray-100" />
+        </div>
+        <div className="w-[98px] h-[98px] rounded-block bg-gray-100" />
+      </div>
+      <div className="w-full flex items-center justify-between p-8 gap-8 text-12">
+        <div className="flex items-center justify-start gap-x-8">
+          <div className="w-[20px] h-[20px] rounded-full bg-gray-100" />
+          <div className="w-[42px] h-[18px] rounded-block bg-gray-100" />
+        </div>
+        <div className="w-[85px] h-[18px] rounded-block bg-gray-100" />
+      </div>
+    </div>
+  );
+};
+
+export default SearchResultPostSkeleton;
