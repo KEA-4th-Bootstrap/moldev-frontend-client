0dcd58ab7a82fb69de8e750a5cd28cf7157c338d
Ginieee <kaj1226@naver.com>
2024-05-21 21:32:55 +0900

    ✨ [Feat] 모달 등/퇴장 관리하는 useModal 생성

---
+++ src/hooks/common/useModal.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+
+const useModal = (url?: string | -1, onClose?: () => void) => {
+  const navigate = useNavigate();
+  const [isShow, setIsShow] = useState(false);
+  const [childIsShow, setChildIsShow] = useState(false);
+
+  useEffect(() => {
+    setIsShow(true);
+    const timer = setTimeout(() => {
+      setChildIsShow(true);
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!childIsShow) {
+      const timer = setTimeout(() => {
+        url === undefined
+          ? onClose && onClose()
+          : onClose
+            ? onClose()
+            : url === -1
+              ? navigate(-1)
+              : navigate(url);
+      }, 300);
+
+      return () => clearTimeout(timer);
+    }
+  }, [childIsShow, navigate, url, onClose]);
+
+  const onBackgroundClick = () => {
+    setChildIsShow(false);
+  };
+
+  return { isShow, childIsShow, setChildIsShow, onBackgroundClick };
+};
+
+export default useModal;
