f6afb726d2157fe890672ad7134b535ae59b1661
Ginieee <kaj1226@naver.com>
2024-05-24 12:13:45 +0900

    ✨ [Feat] useComment 생성 및 getComment/postComment API 연동

---
+++ src/hooks/postPage/useComment.ts
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { getPostCommentApi, postCommentApi } from '../../api/postApi';
+import { useQuery, useMutation } from 'react-query';
+import { commentType } from '../../data/type';
+
+export const useComment = (postId: number) => {
+  const [content, setContent] = useState('');
+  const [commentList, setCommentList] = useState<commentType[]>([]);
+  const { isLoading: commentIsLoading, error: commentIsError } = useQuery(
+    `comment-${postId}`,
+    () => getPostCommentApi(postId),
+    {
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        console.log('data : ', data);
+        const commentList: commentType[] = [];
+        for (
+          let i = 0;
+          i < data.data.data.commentInfo.commentList.length;
+          i++
+        ) {
+          commentList.push({
+            commentInfo: {
+              ...data.data.data.commentInfo.commentList[i],
+            },
+            userInfo: {
+              ...data.data.data.userInfo.userList[i],
+            },
+          });
+        }
+        setCommentList(commentList);
+      },
+      onError: (error) => {
+        console.log('error : ', error);
+      },
+    },
+  );
+
+  const { mutate: tryPostComment } = useMutation(
+    () => postCommentApi(postId, content),
+    {
+      onSuccess: (data) => {
+        console.log('data : ', data);
+      },
+      onError: (error) => {
+        console.log('error : ', error);
+      },
+    },
+  );
+
+  const onChangeContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const onCommentClick = () => {
+    if (content === '') {
+      return;
+    }
+    console.log('comment : ', content);
+    setContent('');
+    tryPostComment();
+  };
+
+  return {
+    commentList,
+    commentIsLoading,
+    commentIsError,
+    content,
+    onChangeContent,
+    onCommentClick,
+  };
+};
