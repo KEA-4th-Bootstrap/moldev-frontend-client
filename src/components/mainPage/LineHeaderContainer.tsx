664ff56f0d8b7587ed8d9226562f410329e4b247
Ginieee <kaj1226@naver.com>
2024-05-24 11:35:08 +0900

    ✨ [Feat] 트렌딩 게시글 조회 : 로딩 및 에러 처리 레이아웃 구현

--- src/components/mainPage/LineHeaderContainer.tsx
+++ src/components/mainPage/LineHeaderContainer.tsx
@@ -2,10 +2,19 @@ import useLineHeader from '../../hooks/mainPage/useLineHeader';
 import LineHeaderItemContainer from './LineHeaderItemContainer';
 import PostListItemContainer from './PostListItemContainer';
 import IslandListItemContainer from './IslandListItemContainer';
+import PostListItemSkeleton from './PostListItemSkeleton';
+import ErrorContainer from '../common/ErrorContainer';
 
 const LineHeaderContainer = () => {
-  const { postList, islandList, selected, onClickPost, onClickIsland } =
-    useLineHeader();
+  const {
+    postList,
+    islandList,
+    selected,
+    onClickPost,
+    onClickIsland,
+    postListIsLoading,
+    postListIsError,
+  } = useLineHeader();
   return (
     <div className="w-full grow flex flex-col items-start justify-start gap-y-20">
       <div className="w-full flex items-center justify-start px-16 border-b border-gray-100">
@@ -21,13 +30,29 @@ const LineHeaderContainer = () => {
         />
       </div>
       <div className="w-full grid grid-cols-3 gap-18 px-16">
-        {selected === 'post'
-          ? postList.map((item, index) => (
+        {selected === 'post' ? (
+          postListIsLoading ? (
+            <>
+              <PostListItemSkeleton />
+              <PostListItemSkeleton />
+              <PostListItemSkeleton />
+            </>
+          ) : postListIsError ? (
+            <>
+              <div />
+              <ErrorContainer />
+              <div />
+            </>
+          ) : (
+            postList.map((item, index) => (
               <PostListItemContainer key={index} item={item} />
             ))
-          : islandList.map((item, index) => (
-              <IslandListItemContainer key={index} item={item} />
-            ))}
+          )
+        ) : (
+          islandList.map((item, index) => (
+            <IslandListItemContainer key={index} item={item} />
+          ))
+        )}
       </div>
     </div>
   );
