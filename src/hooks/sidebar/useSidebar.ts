dba9f9199ed9a29f4552f3189a69201b0aa3997f
Ginieee <kaj1226@naver.com>
2024-05-24 11:40:51 +0900

    ✨ [Feat] useSidebar에서의 zustand > useAuthStore > isLoggedIn 을 활용한 로그인 여부 판별

--- src/hooks/sidebar/useSidebar.ts
+++ src/hooks/sidebar/useSidebar.ts
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { sideType } from '../../data/type';
+import useAuthStore from '../../store/useAuthStore';
 
 const useSidebar = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const { isLoggedIn: isLogin } = useAuthStore();
   const [clicked, setClicked] = useState<sideType>('onboarding');
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isLogoutOpen, setIsLogoutOpen] = useState(false);
   const [isMyPageOpen, setIsMyPageOpen] = useState(false);
   const memberName = '두두';
   const islandNamd = '복복두더지';
-
-  useEffect(() => {
-    setIsLogin(false);
-  }, []);
 
   return {
     isLogin,
