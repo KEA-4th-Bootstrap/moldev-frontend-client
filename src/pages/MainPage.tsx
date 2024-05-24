a73c52da87debadd7360a3cd6761168ed64fc5f9
Ginieee <kaj1226@naver.com>
2024-05-24 11:37:19 +0900

    ✨ [Feat] 트렌딩 게시글 조회 : 로그인 여부 확인을 위한 콘솔 로그 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import MainContainer from '../components/mainPage/MainContainer';
 import SidebarContainer from '../components/sidebar/SidebarContainer';
+import useAuthStore from '../store/useAuthStore';
+import { useEffect } from 'react';
 
 const MainPage = () => {
+  const { isLoggedIn } = useAuthStore();
+  useEffect(() => {
+    console.log('isLoggedIn : ', isLoggedIn);
+  }, [isLoggedIn]);
   return (
     <div className="flex w-full min-h-screen items-center justify-end">
       <SidebarContainer />
