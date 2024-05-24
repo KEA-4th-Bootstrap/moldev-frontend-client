066aee86bdbad1a7d453bf3c840c525acdb5ac10
Ginieee <kaj1226@naver.com>
2024-05-24 11:36:34 +0900

    ✨ [Feat] 트렌딩 게시글 조회 : useLineHeader에 트렌딩 포스트 조회 api 연결

--- src/hooks/mainPage/useLineHeader.ts
+++ src/hooks/mainPage/useLineHeader.ts
@@ -5,11 +5,43 @@ import {
   postListItemType,
 } from '../../data/type';
 import { dummyIslandList, dummyProject } from '../../data/dummy';
+import { useQuery } from 'react-query';
+import { getTrendingPostListApi } from '../../api/mainApi';
 
 const useLineHeader = () => {
   const [selected, setSelected] = useState<lineHeaderType>('post');
   const [postList, setPostList] = useState<postListItemType[]>([]);
   const [islandList, setIslandList] = useState<islandListItemType[]>([]);
+
+  const { isLoading: postListIsLoading, error: postListIsError } = useQuery(
+    'postList',
+    () => getTrendingPostListApi(),
+    {
+      enabled: selected === 'post',
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        console.log('data : ', data);
+        console.log(data.data.data.postInfo);
+        console.log(postListIsLoading);
+        console.log(postListIsError);
+        const dataList: postListItemType[] = [];
+        for (let i = 0; i < data.data.data.postInfo.postList.length; i++) {
+          dataList.push({
+            postInfo: {
+              ...data.data.data.postInfo.postList[i].postInfo,
+            },
+            userInfo: {
+              ...data.data.data.userInfo.userList[i],
+            },
+          });
+        }
+        setPostList(dataList);
+      },
+      onError: (error) => {
+        console.log('error : ', error);
+      },
+    },
+  );
 
   useEffect(() => {
     setPostList(dummyProject);
@@ -30,6 +62,8 @@ const useLineHeader = () => {
     selected,
     onClickPost,
     onClickIsland,
+    postListIsLoading,
+    postListIsError,
   };
 };
 
