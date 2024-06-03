4829dc9bc7de351e4c3ffd4993da4ba9c2be947d
Ginieee <kaj1226@naver.com>
2024-06-03 21:44:23 +0900

    ✨ [Feat] 마이페이지 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import useModal from '../hooks/common/useModal';
+import ModalBackground from '../components/common/ModalBackground';
+import MyPageContainer from '../components/myPage/MyPageContainer';
 
-const MyPage = () => {
-  return <div>마이페이지입니다</div>;
+const MyPage = ({ closeHandler }: { closeHandler: () => void }) => {
+  const { isShow, childIsShow, onBackgroundClick } = useModal('', closeHandler);
+  return (
+    <ModalBackground isShow={isShow} onClick={onBackgroundClick}>
+      <MyPageContainer isShow={childIsShow} onClose={onBackgroundClick} />
+    </ModalBackground>
+  );
 };
 
 export default MyPage;
