9d9f1a46b7b343b39b92ad591cb6183bd6e4a81a
Ginieee <kaj1226@naver.com>
2024-06-03 21:52:19 +0900

    💄 [Fix] 이 카테고리의 다른 게시글 레이아웃 수정

--- src/components/postPage/RecentListItemContainer.tsx
+++ src/components/postPage/RecentListItemContainer.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { recentListItemType } from '../../data/type';
+import { useDateFormat } from '../../hooks/common/useDateFormat';
+import { useNavigate } from 'react-router-dom';
 
 const RecentListItemContainer = ({
+  moldevId,
   item,
-  // isActive,
+  isActive,
 }: {
+  moldevId: string;
   item: recentListItemType;
-  // isActive: boolean;
+  isActive: boolean;
 }) => {
+  const date = useDateFormat(item.updateDate);
+  const navigate = useNavigate();
   return (
     <div
-      className={`w-full flex items-center justify-between text-18 ${item.isActive ? 'text-black' : 'text-gray-200'}`}
+      className={`w-full flex items-center justify-between text-18 ${isActive ? 'text-black' : 'text-gray-200'} hover:underline underline-offset-4 cursor-pointer`}
+      onClick={() => navigate(`/${moldevId}/${item.id}`, { replace: true })}
     >
-      <div className={`${item.isActive ? 'font-semibold' : 'font-medium'}`}>
+      <div className={`${isActive ? 'font-semibold' : 'font-medium'}`}>
         {item.title}
       </div>
-      <div>{item.createdAt}</div>
+      <div>{date}</div>
     </div>
   );
 };
