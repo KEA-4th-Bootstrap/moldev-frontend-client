13fe4880919946bf2717dbd2c7f2d8e1c0b9721e
Ginieee <kaj1226@naver.com>
2024-06-03 10:21:28 +0900

    [Feat] 유저 정보 조회 등 멤버 관련 API 코드 구현

--- src/api/memberApi.ts
+++ src/api/memberApi.ts
@@ -1,5 +1,37 @@
-import { baseAxios } from './axiosInstance';
+import { authAxios, baseAxios } from './axiosInstance';
 
-export const patchPassword = async (password: string) => {
-  return baseAxios.patch('/api/member/password', { password });
+export const patchPassword = async (email: string, password: string) => {
+  return baseAxios.patch('/api/member/password', {
+    email: email,
+    password: password,
+  });
+};
+
+export const getUserInfo = async (moldevId: string) => {
+  return baseAxios.get(`/api/member/${moldevId}/profile`);
+};
+
+export const getMyInfo = async () => {
+  return authAxios.get('/api/member/my');
+};
+
+export const patchProfileImage = async (profileImage: File) => {
+  // const formData = new FormData();
+  // formData.append('profileImage', profileImage);
+
+  return authAxios.patch(
+    '/api/member/profile-image',
+    {
+      profileImage: profileImage,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+};
+
+export const patchProfile = async (nickname: string, islandName: string) => {
+  return authAxios.patch('/api/member/my', { nickname, islandName });
 };
