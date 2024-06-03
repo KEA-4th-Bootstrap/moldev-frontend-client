abd8f346c9228e4c45bc86bbb18aec42546dd7ad
Ginieee <kaj1226@naver.com>
2024-06-03 13:34:06 +0900

    ✨  [Feat] 카테고리 변경 훅 useCategoryList 구현

--- src/hooks/categoryPage/useCategoryList.ts
+++ src/hooks/categoryPage/useCategoryList.ts
@@ -1,11 +1,32 @@
 import { useParams } from 'react-router';
 import useModal from '../common/useModal';
+import { categoryType } from '../../data/type';
 
 const useCategoryList = () => {
   const { moldevId, categoryName } = useParams();
   const { isShow, childIsShow, onBackgroundClick } = useModal(`/${moldevId}`);
 
-  return { isShow, childIsShow, onBackgroundClick, categoryName };
+  const changeCategoryType = (category: string): categoryType => {
+    if (category === 'activity') {
+      return 'ACTIVITY';
+    } else if (category === 'project') {
+      return 'PROJECT';
+    } else if (category === 'awards') {
+      return 'AWARDS';
+    } else if (category === 'trouble') {
+      return 'TROUBLE';
+    } else {
+      return 'ACTIVITY';
+    }
+  };
+  const realCategoryName = changeCategoryType(categoryName || '');
+
+  return {
+    isShow,
+    childIsShow,
+    onBackgroundClick,
+    realCategoryName,
+  };
 };
 
 export default useCategoryList;
