91f6cbdb4d502af26d53c23f98abfd6902c7431c
Ginieee <kaj1226@naver.com>
2024-06-03 10:26:03 +0900

    ✨ [Feat] 유저 추천 페이지 API 연동

--- src/pages/sidebar/RecommendPage.tsx
+++ src/pages/sidebar/RecommendPage.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { ReactComponent as Refresh } from '../../assets/icons/icon_refresh_gray_600.svg';
 import EmptyRecommend from '../../components/sidebar/recommend/EmptyRecommend';
 import useRecommend from '../../hooks/sidebar/recommend/useRecommend';
-import RecommendListItemContainer from '../../components/sidebar/recommend/RecommendListItemContainer';
+import ErrorContainer from '../../components/common/ErrorContainer';
+import SearchResultIslandContainer from '../../components/sidebar/search/SearchResultIslandContainer';
+import SearchResultIslandSkeleton from '../../components/sidebar/search/SearchResultIslandSkeleton';
+import NeedLoginContainer from '../../components/common/NeedLoginContainer';
+import { getNickname } from '../../api/manageLocalStorage';
 
 const RecommendPage = () => {
-  const { recommend } = useRecommend(1);
-  return (
+  const { recommend, isLoading, isFetching, isError, isLoggedIn, refetch } =
+    useRecommend();
+  const nickname = getNickname();
+  return isLoggedIn ? (
     <div className="grow h-full flex flex-col items-start justify-start">
       <div className="w-full mt-70 pb-30 px-16 text-28 flex flex-col gap-y-2 border-b-[0.5px] border-gray-100">
         <div className="flex items-center justify-start gap-x-6">
-          <div className="font-bold">챗봇</div>
+          <div className="font-bold">{nickname ? nickname : '당신'}</div>
           <div>님과</div>
         </div>
         <div className="flex items-center justify-start gap-x-6">
@@ -20,19 +26,38 @@ const RecommendPage = () => {
       <div className="w-full grow flex flex-col gap-y-20 px-16 py-20 items-start justify-start">
         <div className="w-full flex items-center justify-between">
           <div className="font-medium text-12 text-gray-600">추천 섬</div>
-          <Refresh width={18} height={18} />
+          <Refresh
+            className="cursor-pointer"
+            width={18}
+            height={18}
+            onClick={() => refetch()}
+          />
         </div>
-        <div className="w-full grow flex flex-col items-center justify-start">
-          {recommend.length === 0 ? (
+        <div className="w-full grow flex flex-col items-center justify-start gap-y-16">
+          {!recommend || isFetching ? (
+            isLoading || isFetching ? (
+              <>
+                <SearchResultIslandSkeleton />
+                <SearchResultIslandSkeleton />
+                <SearchResultIslandSkeleton />
+              </>
+            ) : isError ? (
+              <ErrorContainer />
+            ) : (
+              <div>알 수 없는 에러가 발생하였습니다.</div>
+            )
+          ) : recommend.length < 1 ? (
             <EmptyRecommend />
           ) : (
             recommend.map((item, index) => (
-              <RecommendListItemContainer key={index} item={item} />
+              <SearchResultIslandContainer key={index} item={item} />
             ))
           )}
         </div>
       </div>
     </div>
+  ) : (
+    <NeedLoginContainer />
   );
 };
 
