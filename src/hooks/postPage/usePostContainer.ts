57240c48598df2ed02bab27de8336859c49233fd
Ginieee <kaj1226@naver.com>
2024-06-03 15:07:47 +0900

    ✨ [Feat] usePostContainr에 onMoveToEdit 및 deletePost 연결

--- src/hooks/postPage/usePostContainer.ts
+++ src/hooks/postPage/usePostContainer.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
 import usePost from './usePost';
 import { ContentState, EditorState } from 'draft-js';
-import { recentListItemType } from '../../data/type';
 import htmlToDraft from 'html-to-draftjs';
-import { dummyRecentList } from '../../data/dummy';
+import { useMutation } from 'react-query';
+import { deletePostApi } from '../../api/postApi';
+import { useNavigate } from 'react-router-dom';
 
 export const usePostContainer = (moldevId: string, postId: number) => {
   const { post, postIsLoading, postIsError } = usePost(moldevId, postId);
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [recentList, setRecentList] = useState<recentListItemType[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!post) return;
     const blocksFromHtml = htmlToDraft(post.postInfo.content);
+    console.log('HTML TO DRAFT --> ', blocksFromHtml);
     if (blocksFromHtml) {
       const { contentBlocks, entityMap } = blocksFromHtml;
       console.log(contentBlocks, entityMap);
@@ -24,16 +26,31 @@ export const usePostContainer = (moldevId: string, postId: number) => {
     }
   }, [post]);
 
-  useEffect(() => {
-    setRecentList(dummyRecentList);
-  }, []);
+  const onMoveToEdit = (postId: number) => {
+    console.log('게시글 수정으로 이동 --> ', postId);
+    navigate(`/edit/${postId}`);
+  };
+
+  const { mutate: tryDeletePost } = useMutation(
+    (postId: number) => deletePostApi(postId),
+    {
+      onSuccess: (data) => {
+        console.log('게시글 삭제 성공 --> ', data);
+        navigate(-1);
+      },
+      onError: (error) => {
+        console.log('게시글 삭제 실패 --> ', error);
+      },
+    },
+  );
 
   return {
     post,
     postIsLoading,
     postIsError,
     editorState,
-    recentList,
     setEditorState,
+    onMoveToEdit,
+    tryDeletePost,
   };
 };
