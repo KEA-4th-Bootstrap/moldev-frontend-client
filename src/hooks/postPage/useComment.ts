460270fe31dc9d0c9c3b30675d88d717d383eeb7
Ginieee <kaj1226@naver.com>
2024-06-03 15:11:13 +0900

    ✨ [Feat] useComment에 댓글 조회 API 연동

--- src/hooks/postPage/useComment.ts
+++ src/hooks/postPage/useComment.ts
@@ -6,41 +6,38 @@ import { commentType } from '../../data/type';
 export const useComment = (postId: number) => {
   const [content, setContent] = useState('');
   const [commentList, setCommentList] = useState<commentType[]>([]);
-  const { isLoading: commentIsLoading, error: commentIsError } = useQuery(
-    `comment-${postId}`,
-    () => getPostCommentApi(postId),
-    {
-      refetchOnWindowFocus: false,
-      onSuccess: (data) => {
-        console.log('data : ', data);
-        const commentList: commentType[] = [];
-        for (
-          let i = 0;
-          i < data.data.data.commentInfo.commentList.length;
-          i++
-        ) {
-          commentList.push({
-            commentInfo: {
-              ...data.data.data.commentInfo.commentList[i],
-            },
-            userInfo: {
-              ...data.data.data.userInfo.userList[i],
-            },
-          });
-        }
-        setCommentList(commentList);
-      },
-      onError: (error) => {
-        console.log('error : ', error);
-      },
+  const {
+    isLoading: commentIsLoading,
+    error: commentIsError,
+    refetch: commentRefetch,
+  } = useQuery(`comment-${postId}`, () => getPostCommentApi(postId), {
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      console.log('data : ', data);
+      const commentList: commentType[] = [];
+      for (let i = 0; i < data.data.data.commentInfo.commentList.length; i++) {
+        commentList.push({
+          commentInfo: {
+            ...data.data.data.commentInfo.commentList[i],
+          },
+          userInfo: {
+            ...data.data.data.userInfo.userList[i],
+          },
+        });
+      }
+      setCommentList(commentList);
     },
-  );
+    onError: (error) => {
+      console.log('error : ', error);
+    },
+  });
 
   const { mutate: tryPostComment } = useMutation(
     () => postCommentApi(postId, content),
     {
       onSuccess: (data) => {
         console.log('data : ', data);
+        commentRefetch();
       },
       onError: (error) => {
         console.log('error : ', error);
