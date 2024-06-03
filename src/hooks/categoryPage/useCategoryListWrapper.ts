6914c07315076c1dbd497fd6022393aa07546e97
Ginieee <kaj1226@naver.com>
2024-06-03 13:35:34 +0900

    ✨ [Feat] 미션스크롤 카테고리 API 연동

--- src/hooks/categoryPage/useCategoryListWrapper.ts
+++ src/hooks/categoryPage/useCategoryListWrapper.ts
@@ -1,26 +1,22 @@
 import { useParams } from 'react-router';
 import { useEffect, useRef, useState } from 'react';
 import {
-  dummyActivity,
-  dummyAwards,
-  dummyProject,
-  dummyTrouble,
-} from '../../data/dummy';
-import {
   ItemDimensions,
   categoryType,
-  postListItemType,
+  postListItemPostType,
 } from '../../data/type';
 import { MissionControl } from './MissionControl';
+import { useQuery } from 'react-query';
+import { getMissionControl } from '../../api/postApi';
 
 const useCategoryListWrapper = (category: categoryType) => {
   const { moldevId } = useParams();
-  const [listItems, setListItems] = useState<postListItemType[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(1);
   const outerRef = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [itemDimensions, setItemDimensions] = useState<ItemDimensions[][]>([]);
+  const [page, setPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
     console.log('list wrapper moldevId', moldevId);
@@ -30,8 +26,8 @@ const useCategoryListWrapper = (category: categoryType) => {
     if (!outerRef.current) return;
     // outerRef size
     const { width, height } = outerRef.current.getBoundingClientRect();
-    console.log('outerRef', outerRef.current.className);
-    console.log('outerRef', width, height);
+    // console.log('outerRef', outerRef.current.className);
+    // console.log('outerRef', width, height);
     setWidth(width);
     setHeight(height);
   }, [outerRef, outerRef.current?.onresize]);
@@ -42,34 +38,51 @@ const useCategoryListWrapper = (category: categoryType) => {
     const itemDimensions = MissionControl(width, height, 5, 32);
     console.log('itemDimensions', itemDimensions);
     setItemDimensions(itemDimensions);
-  }, [width, height, listItems]);
+  }, [width, height]);
 
-  useEffect(() => {
-    switch (category) {
-      case 'ACTIVITY':
-        setListItems(dummyActivity);
-        break;
-      case 'PROJECT':
-        setListItems(dummyProject);
-        break;
-      case 'AWARDS':
-        setListItems(dummyAwards);
-        break;
-      case 'TROUBLE':
-        setListItems(dummyTrouble);
-        break;
-      default:
-        setListItems([]);
-    }
-  }, [category]);
+  const handlePageChange = (page: number) => {
+    if (page < 0 || page >= totalPage) return;
+    setPage(page);
+  };
+
+  // const handleOtherPageChange = (diff: number) => {
+  //   setPage((prev) => prev + diff);
+  // };
+
+  const {
+    data: categoryData,
+    isError,
+    isLoading,
+  } = useQuery<postListItemPostType[]>(
+    ['category', category, page],
+    () =>
+      getMissionControl(moldevId || '', category, page, 5).then((res) => {
+        console.log(`미션 컨트롤 ${category} 성공 --> `, res.data.data.data);
+        setTotalPage(res.data.data.data.pageInfo.totalPages);
+        return res.data.data.data.postList;
+      }),
+    {
+      enabled: !!category && !!moldevId,
+      onSuccess: (data) => {
+        console.log(`미션 컨트롤 ${category} 성공 성공 --> `, data);
+      },
+      onError: (error) => {
+        console.log(`미션 컨트롤 ${category} 실패 --> `, error);
+      },
+    },
+  );
 
   return {
-    listItems,
-    currentIndex,
-    setCurrentIndex,
     outerRef,
     itemDimensions,
     moldevId,
+    categoryData,
+    isError,
+    isLoading,
+    page,
+    totalPage,
+    handlePageChange,
+    // handleOtherPageChange,
   };
 };
 
