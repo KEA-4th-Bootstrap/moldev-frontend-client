b1a165200478a6cfa3790300c553ce399003d72e
Ginieee <kaj1226@naver.com>
2024-06-03 11:11:54 +0900

    ✨ [Feat] useSearch에 검색 API 연동

--- src/hooks/sidebar/search/useSearch.ts
+++ src/hooks/sidebar/search/useSearch.ts
@@ -1,16 +1,97 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
-  islandListItemType,
   postListItemType,
+  postListItemUserType,
   searchOptionType,
 } from '../../../data/type';
-import { dummyIslandList, dummyProject } from '../../../data/dummy';
+import { useInfiniteQuery } from 'react-query';
+import { getSearchIslandApi, getSearchPostApi } from '../../../api/searchApi';
 
 const useSearch = () => {
   const [search, setSearch] = useState('');
   const [option, setOption] = useState<searchOptionType[]>(['post', 'island']);
   const [postList, setPostList] = useState<postListItemType[]>([]);
-  const [islandList, setIslandList] = useState<islandListItemType[]>([]);
+  const [islandList, setIslandList] = useState<postListItemUserType[]>([]);
+
+  const {
+    refetch: refetchSearchPost,
+    hasNextPage: searchPostHasNextPage,
+    isLoading: searchPostIsLoading,
+    isError: searchPostIsError,
+    fetchNextPage: fetchNextPost,
+    isFetchingNextPage: searchPostIsFetchingNextPage,
+  } = useInfiniteQuery(
+    ['searchPost', search],
+    ({ pageParam = 0 }) => getSearchPostApi(search, pageParam, 10),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.data.data.postInfo.pageInfo.hasNextPage) {
+          return lastPage.data.data.postInfo.pageInfo.pageNumber + 1;
+        }
+        return undefined;
+      },
+      enabled: false,
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        console.log('포스트 검색 성공 --> ', data);
+        // console.log('포스트 검색 성공 --> ', data.pageParams);
+        if (data.pages) {
+          const postList = data.pages.map((page) => {
+            const postListItems: postListItemType[] = [];
+            for (let i = 0; i < page.data.data.postInfo.postList.length; i++) {
+              const item: postListItemType = {
+                postInfo: page.data.data.postInfo.postList[i],
+                userInfo: page.data.data.userInfo.userList[i],
+              };
+              postListItems.push(item);
+            }
+
+            return postListItems;
+          });
+          setPostList(postList.flat());
+        }
+      },
+      onError: (error) => {
+        console.error('포스트 검색 실패 --> ', error);
+      },
+    },
+  );
+
+  useEffect(() => {
+    console.log('hasNextPost : ', searchPostHasNextPage);
+  }, [searchPostHasNextPage]);
+
+  const {
+    refetch: refetchSearchIsland,
+    hasNextPage: searchIslandHasNextPage,
+    isLoading: searchIslandIsLoading,
+    isError: searchIslandIsError,
+    fetchNextPage: fetchNextIsland,
+    isFetchingNextPage: searchIslandIsFetchingNextPage,
+  } = useInfiniteQuery(
+    ['searchIsland', search],
+    ({ pageParam = 0 }) => getSearchIslandApi(search, pageParam, 10),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.data.data.pageInfo.hasNextPage) {
+          return lastPage.data.data.pageInfo.pageNumber + 1;
+        }
+        return undefined;
+      },
+      enabled: false,
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        console.log('섬 검색 성공 --> ', data);
+        if (data.pages) {
+          const island = data.pages.map((page) => page.data.data.searchList);
+          setIslandList(island.flat());
+        }
+      },
+      onError: (error) => {
+        console.error('섬 검색 실패 --> ', error);
+      },
+    },
+  );
 
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -18,6 +99,8 @@ const useSearch = () => {
 
   const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
+      setPostList([]);
+      setIslandList([]);
       handleSearch();
     }
   };
@@ -35,11 +118,12 @@ const useSearch = () => {
   };
 
   const handlePostList = () => {
-    setPostList(dummyProject);
+    console.log('포스트 리스트 불러오기');
+    refetchSearchPost();
   };
 
   const handleIslandList = () => {
-    setIslandList(dummyIslandList);
+    refetchSearchIsland();
   };
 
   const handleSearch = () => {
@@ -60,6 +144,18 @@ const useSearch = () => {
     islandList,
     handleKeydown,
     handleOnlyOption,
+    refetchSearchPost,
+    searchPostHasNextPage,
+    searchPostIsLoading,
+    searchPostIsError,
+    fetchNextPost,
+    searchPostIsFetchingNextPage,
+    refetchSearchIsland,
+    searchIslandHasNextPage,
+    searchIslandIsLoading,
+    searchIslandIsError,
+    fetchNextIsland,
+    searchIslandIsFetchingNextPage,
   };
 };
 
