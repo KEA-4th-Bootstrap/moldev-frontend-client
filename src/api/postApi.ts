fd2a5c7fe8e86887dd16eac7c536d30c625b9d7f
Ginieee <kaj1226@naver.com>
2024-06-03 10:21:04 +0900

    ✨ [Feat] 게시글 삭제 등 포스트 관련 API 코드 구현

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,3 +1,4 @@
+import { categoryType } from '../data/type';
 import { authAxios, baseAxios } from './axiosInstance';
 
 export const getPostApi = async (moldevId: string, postId: number) => {
@@ -28,5 +29,107 @@ export const postReplyApi = async (
     postId: postId,
     parentsId: parentsId,
     content: content,
+  });
+};
+
+export const deleteCommentApi = async (commentId: string) => {
+  return authAxios.delete(`/api/reply/${commentId}`);
+};
+
+export const postImageApi = async (image: File) => {
+  // const formData = new FormData();
+  // formData.append('image', image);
+  return authAxios.post(
+    `/api/post/image`,
+    {
+      thumbnail: image,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+};
+
+export const postWriteApi = async (
+  title: string,
+  moldevId: string,
+  content: string,
+  profileContent: string,
+  thumbnail: string,
+  images: string[],
+  category: string,
+) => {
+  return authAxios.post('/api/post', {
+    title: title,
+    moldevId: moldevId,
+    content: content,
+    profileContent: profileContent,
+    thumbnail: thumbnail,
+    images: images,
+    category: category,
+  });
+};
+
+export const patchPostApi = async (
+  postId: number,
+  title: string,
+  moldevId: string,
+  content: string,
+  profileContent: string,
+  thumbnail: string,
+  category: string,
+) => {
+  return authAxios.patch(`/api/post/${postId}`, {
+    title: title,
+    moldevId: moldevId,
+    content: content,
+    profileContent: profileContent,
+    thumbnail: thumbnail,
+    category: category,
+  });
+};
+
+export const deletePostApi = async (postId: number) => {
+  return authAxios.delete(`/api/post/${postId}`, {
+    data: {},
+  });
+};
+
+export const getPostImagesApi = async (postId: number) => {
+  return authAxios.get(`/api/post/${postId}/images`);
+};
+
+export const getMissionControl = async (
+  moldevId: string,
+  category: categoryType,
+  page?: number,
+  size?: number,
+) => {
+  return baseAxios.get(`/api/post/mission-control`, {
+    params: {
+      moldevId: moldevId,
+      type: category,
+      page: page ? page : 0,
+      size: size ? size : 5,
+    },
+  });
+};
+
+export const getPostRecentListApi = async (
+  moldevId: string,
+  postId: number,
+  category: categoryType,
+  preCount: number,
+  postCount: number,
+) => {
+  return baseAxios.get(`/api/post/${postId}/category/list`, {
+    params: {
+      moldevId: moldevId,
+      type: category,
+      preCount: preCount,
+      postCount: postCount,
+    },
   });
 };
