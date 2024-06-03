67b45a150f364ada7055d2274e9573b09b6bcd27
Ginieee <kaj1226@naver.com>
2024-06-03 21:58:35 +0900

    💄 [Fix] 미션컨트롤 및 아이템 레이아웃 수정

--- src/components/categoryPage/CategoryListItemContainer.tsx
+++ src/components/categoryPage/CategoryListItemContainer.tsx
@@ -1,4 +1,4 @@
-import { postListItemType } from '../../data/type';
+import { postListItemPostType } from '../../data/type';
 import useRouteNavigate from '../../hooks/common/useRouteNavigate';
 
 const CategoryListItemContainer = ({
@@ -7,12 +7,12 @@ const CategoryListItemContainer = ({
   width,
   height,
 }: {
-  item: postListItemType;
+  item: postListItemPostType;
   moldevId: string;
   width: number;
   height: number;
 }) => {
-  const { onClick } = useRouteNavigate(`/${moldevId}/${item.postInfo.id}`);
+  const { onClick } = useRouteNavigate(`/${moldevId}/${item.id}`);
 
   return (
     <div
@@ -23,18 +23,16 @@ const CategoryListItemContainer = ({
       <div className="shrink w-full grow rounded-t-md overflow-hidden">
         <img
           className="shrink w-full h-full object-cover"
-          src={item.postInfo.thumbnail}
-          alt={item.postInfo.title}
+          src={item.thumbnail}
+          alt={item.title}
         />
       </div>
       <div className="w-full flex flex-col items-start justify-between bg-white px-16 py-13">
-        <div className="shrink-0 w-full font-bold text-16">
-          {item.postInfo.title}
-        </div>
+        <div className="shrink-0 w-full font-bold text-16">{item.title}</div>
         <div className={`shrink-0 w-full grow text-14 line-clamp-1`}>
-          {item.postInfo.content}
+          {item.content}
         </div>
-        <div className="shrink-0 w-full flex items-center justify-end text-12 text-gray-800">{`조회수 ${item.postInfo.viewCount}회 | ${item.postInfo.lastModifiedDate}`}</div>
+        <div className="shrink-0 w-full flex items-center justify-end text-12 text-gray-800">{`조회수 ${item.viewCount}회 | ${item.lastModifiedDate}`}</div>
       </div>
     </div>
   );
--- src/components/categoryPage/CategoryListWrapper.tsx
+++ src/components/categoryPage/CategoryListWrapper.tsx
@@ -1,5 +1,8 @@
 import { categoryType } from '../../data/type';
 import useCategoryListWrapper from '../../hooks/categoryPage/useCategoryListWrapper';
+import EmptyContainer from '../common/EmptyContainer';
+import ErrorContainer from '../common/ErrorContainer';
+import LoadingSpinner from '../common/LoadingSpinner';
 import CategoryListItemContainer from './CategoryListItemContainer';
 
 const CategoryListWrapper = ({
@@ -10,12 +13,16 @@ const CategoryListWrapper = ({
   isShow: boolean;
 }) => {
   const {
-    listItems,
-    currentIndex,
-    setCurrentIndex,
     outerRef,
     itemDimensions,
     moldevId,
+    categoryData,
+    isError,
+    isLoading,
+    page,
+    totalPage,
+    handlePageChange,
+    // handleOtherPageChange,
   } = useCategoryListWrapper(category);
 
   return (
@@ -24,35 +31,49 @@ const CategoryListWrapper = ({
         ref={outerRef}
         className={`w-2/3 grow flex flex-col items-center justify-center gap-y-30 py-80 ${isShow ? 'translate-y-0' : 'translate-y-[200%]'} transition-all duration-150`}
       >
-        {listItems.length > 0 && itemDimensions.length > 0 && (
-          <>
-            <div className="w-full flex items-end justify-center gap-x-30">
-              {listItems
-                .slice(0, itemDimensions[0].length)
-                .map((item, index) => (
-                  <CategoryListItemContainer
-                    key={item.postInfo.id}
-                    item={item}
-                    moldevId={moldevId || ''}
-                    width={itemDimensions[0][index].width}
-                    height={itemDimensions[0][index].height}
-                  />
-                ))}
+        {!categoryData ? (
+          isLoading ? (
+            <LoadingSpinner />
+          ) : isError ? (
+            <ErrorContainer />
+          ) : (
+            <div className="w-full flex items-center justify-center">
+              알 수 없는 오류가 발생했습니다.
             </div>
-            <div className="w-full flex items-start justify-center gap-x-30">
-              {listItems
-                .slice(itemDimensions[0].length, 5)
-                .map((item, index) => (
-                  <CategoryListItemContainer
-                    key={item.postInfo.id}
-                    item={item}
-                    moldevId={moldevId || ''}
-                    width={itemDimensions[1][index].width}
-                    height={itemDimensions[1][index].height}
-                  />
-                ))}
-            </div>
-          </>
+          )
+        ) : categoryData.length < 1 ? (
+          <EmptyContainer />
+        ) : (
+          itemDimensions.length > 0 && (
+            <>
+              <div className="w-full flex items-end justify-center gap-x-30">
+                {categoryData
+                  .slice(0, itemDimensions[0].length)
+                  .map((item, index) => (
+                    <CategoryListItemContainer
+                      key={item.id}
+                      item={item}
+                      moldevId={moldevId || ''}
+                      width={itemDimensions[0][index].width}
+                      height={itemDimensions[0][index].height}
+                    />
+                  ))}
+              </div>
+              <div className="w-full flex items-start justify-center gap-x-30">
+                {categoryData
+                  .slice(itemDimensions[0].length, 5)
+                  .map((item, index) => (
+                    <CategoryListItemContainer
+                      key={item.id}
+                      item={item}
+                      moldevId={moldevId || ''}
+                      width={itemDimensions[1][index].width}
+                      height={itemDimensions[1][index].height}
+                    />
+                  ))}
+              </div>
+            </>
+          )
         )}
       </div>
       <div
@@ -62,14 +83,16 @@ const CategoryListWrapper = ({
         }}
       >
         <button
-          onClick={() => setCurrentIndex(currentIndex - 1)}
-          disabled={currentIndex === 1}
+          className="text-dark-50 py-8 px-16 rounded-block bg-white/50"
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
         >
           이전
         </button>
         <button
-          onClick={() => setCurrentIndex(currentIndex + 1)}
-          disabled={currentIndex * 5 >= listItems.length}
+          className="text-dark-50 py-8 px-16 rounded-block bg-white/50"
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPage}
         >
           다음
         </button>
