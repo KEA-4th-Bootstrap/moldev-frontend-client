5d5f627f58c568ba6ef9bf01816c53003ee20930
Ginieee <kaj1226@naver.com>
2024-06-03 15:10:22 +0900

    ✨ [Feat] useCommentItemWrapper 에 댓글 삭제 API 연동

---
+++ src/hooks/postPage/useCommentItemWrapper.ts
@@ -0,0 +1,28 @@
+import { useMutation } from 'react-query';
+import { getMoldevId } from '../../api/manageLocalStorage';
+import { commentType, replyType } from '../../data/type';
+import { useDateFormat } from '../common/useDateFormat';
+import { deleteCommentApi } from '../../api/postApi';
+
+export const useCommentItemWrapper = (comment: commentType | replyType) => {
+  const moldevId = getMoldevId();
+  const info =
+    'commentInfo' in comment ? comment.commentInfo : comment.replyInfo;
+  const userInfo = comment.userInfo;
+  const date = useDateFormat(info.createdAt);
+
+  const { mutate: deleteComment } = useMutation(
+    () => deleteCommentApi(info.id),
+    {
+      onSuccess: (data) => {
+        console.log('댓글 삭제 성공 --> ', data);
+        window.location.reload();
+      },
+      onError: (error) => {
+        console.log('댓글 삭제 실패 --> ', error);
+      },
+    },
+  );
+
+  return { moldevId, info, userInfo, date, deleteComment };
+};
