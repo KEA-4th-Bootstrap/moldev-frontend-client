65686853c21b6eeed5505181bf0aaace7a9e7c30
Ginieee <kaj1226@naver.com>
2024-06-03 10:18:28 +0900

    ✨ [Feat] 검색 API 및 게시글 조회 api 등 코드 구현

--- src/api/mainApi.ts
+++ src/api/mainApi.ts
@@ -1,5 +1,45 @@
-import { baseAxios } from './axiosInstance';
+import { categoryType } from '../data/type';
+import { authAxios, baseAxios } from './axiosInstance';
 
 export const getTrendingPostListApi = async () => {
   return baseAxios.get('/api/compose/post/trend');
+};
+
+export const getTrendingIslandListApi = async () => {
+  return baseAxios.get('/api/compose/post/trend-island');
+};
+
+export const getSidebarPostListApi = async (
+  moldevId: string,
+  category: categoryType,
+  page: number,
+  size: number,
+) => {
+  return baseAxios.get(
+    `/api/post/${moldevId}/category?page=${page}&size=${size}&type=${category}`,
+  );
+};
+
+export const getPostSearchApi = async (
+  title: string,
+  page?: number,
+  size?: number,
+) => {
+  return baseAxios.get(
+    `/api/compose/post/search?title=${title}&page=${page ? page : 0}&size=${size ? size : 3}`,
+  );
+};
+
+export const getIslandSearchApi = async (
+  searchText: string,
+  page?: number,
+  size?: number,
+) => {
+  return baseAxios.get(
+    `/api/member/search?searchText=${searchText}&page=${page ? page : 0}&size=${size ? size : 3}`,
+  );
+};
+
+export const getRecommendIslandApi = async () => {
+  return authAxios.get(`/api/recommend`);
 };
