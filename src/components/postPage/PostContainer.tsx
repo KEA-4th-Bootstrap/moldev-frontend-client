ee3010343a0bade16f0ca51a6ce2015e74969cf7
Ginieee <kaj1226@naver.com>
2024-06-03 21:53:10 +0900

    ✨ [Feat] 포스트페이지에 수정버튼 삽입

--- src/components/postPage/PostContainer.tsx
+++ src/components/postPage/PostContainer.tsx
@@ -3,14 +3,18 @@ import { categoryToKorean } from '../../data/type';
 import Editor from '@draft-js-plugins/editor';
 import { ReactComponent as Url } from '../../assets/icons/icon_url.svg';
 import { ReactComponent as Plane } from '../../assets/icons/icon_plane.svg';
-import { ReactComponent as Prev } from '../../assets/icons/icon_prev.svg';
-import { ReactComponent as Next } from '../../assets/icons/icon_next.svg';
-import RecentListItemContainer from './RecentListItemContainer';
 import CommentContainer from './CommentContainer';
 import { usePostContainer } from '../../hooks/postPage/usePostContainer';
 import LoadingSpinner from '../common/LoadingSpinner';
 import ErrorContainer from '../common/ErrorContainer';
 import { useDateFormat } from '../../hooks/common/useDateFormat';
+import createImagePlugin from '@draft-js-plugins/image';
+import { getMoldevId } from '../../api/manageLocalStorage';
+import RectButton from '../common/RectButton';
+import PostRecentContainer from './PostRecentContainer';
+
+const imagePlugin = createImagePlugin();
+const plugins = [imagePlugin];
 
 const PostContainer = ({
   moldevId,
@@ -21,13 +25,15 @@ const PostContainer = ({
   postId: number;
   isShow: boolean;
 }) => {
+  const myMoldevId = getMoldevId();
   const {
     post,
     postIsLoading,
     postIsError,
     editorState,
-    recentList,
     setEditorState,
+    onMoveToEdit,
+    tryDeletePost,
   } = usePostContainer(moldevId, postId);
   const date = useDateFormat(post?.postInfo.lastModifiedDate);
 
@@ -57,7 +63,7 @@ const PostContainer = ({
               <div className="flex items-center justify-start gap-x-8">
                 <Pin className="h-[16px] w-auto" />
                 <div className="text-14 font-medium text-gray-700">
-                  {categoryToKorean[post.postInfo.category]}
+                  {categoryToKorean[post.postInfo.category || 'ACTIVITY']}
                 </div>
               </div>
             </div>
@@ -67,7 +73,7 @@ const PostContainer = ({
             <div className="w-full flex items-center justify-between">
               <div className="flex items-center justify-start gap-x-6">
                 <img
-                  src={post.postInfo.thumbnail}
+                  src={post.userInfo.profileImgUrl}
                   alt="profile"
                   className="w-[24px] h-[24px] rounded-full"
                 />
@@ -77,10 +83,29 @@ const PostContainer = ({
               </div>
               <div className="font-medium text-14 text-gray-700">{`${date} | 조회수 ${post.postInfo.viewCount}`}</div>
             </div>
+            {myMoldevId === moldevId && (
+              <div className="w-full flex items-center justify-end gap-x-16">
+                <RectButton
+                  type="fill"
+                  text="수정"
+                  onClick={() => onMoveToEdit(post.postInfo.id)}
+                  h="40px"
+                />
+                <RectButton
+                  type="stroke"
+                  text="삭제"
+                  onClick={() => {
+                    tryDeletePost(post.postInfo.id);
+                  }}
+                  h="40px"
+                />
+              </div>
+            )}
           </div>
           <div className="w-full grow px-48 py-32 flex flex-col items-center justify-start">
             <div className="editor flex flex-col items-center justify-start w-full h-full">
               <Editor
+                plugins={plugins}
                 editorState={editorState}
                 readOnly={true}
                 onChange={setEditorState}
@@ -95,25 +120,7 @@ const PostContainer = ({
               <Plane width={24} height={24} />
             </button>
           </div>
-          <div className="w-full flex items-center justify-center border-b border-gray-50">
-            <div className="w-1/2 flex flex-col items-start justify-center py-30">
-              <div className="w-full flex items-center justify-between">
-                <div className="text-20 font-medium">
-                  <span className="font-semibold">{`${categoryToKorean[post.postInfo.category]} `}</span>
-                  카테고리의 다른 글
-                </div>
-                <div className="flex items-center">
-                  <Prev width={24} height={24} />
-                  <Next width={24} height={24} />
-                </div>
-              </div>
-              <div className="mt-36 w-full flex flex-col items-center justify-center gap-y-10">
-                {recentList.map((item, idx) => (
-                  <RecentListItemContainer item={item} key={idx} />
-                ))}
-              </div>
-            </div>
-          </div>
+          <PostRecentContainer post={post} moldevId={moldevId} />
           <CommentContainer postId={postId} />
         </div>
       )}
