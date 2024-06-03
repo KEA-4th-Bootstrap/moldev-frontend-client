3d934c26efb3043211d79a5b98a8c44b6cfb0af8
Ginieee <kaj1226@naver.com>
2024-06-03 21:49:25 +0900

    💄 [Fix] 검색결과 API 연동 중 검색 결과 아이템들 레이아웃 수정

--- src/components/sidebar/search/SearchResultIslandContainer.tsx
+++ src/components/sidebar/search/SearchResultIslandContainer.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { islandListItemType } from '../../../data/type';
+import { postListItemUserType } from '../../../data/type';
+import { useNavigate } from 'react-router-dom';
 
 const SearchResultIslandContainer = ({
   item,
 }: {
-  item: islandListItemType;
+  item: postListItemUserType;
 }) => {
+  const navigate = useNavigate();
   return (
-    <div className="w-full flex flex-col rounded-block bg-white hover:bg-gray-100/40 px-8 py-7">
+    <div
+      className="w-full flex flex-col rounded-block bg-white hover:bg-gray-100/40 px-8 py-7 gap-y-10 cursor-pointer"
+      onClick={() => navigate(`/${item.moldevId}`)}
+    >
       <div className="w-full flex items-center justify-start gap-x-13">
         <img
           className="w-[32px] h-[32px] rounded-full"
-          src={item.userImg}
-          alt={item.userName}
+          src={item.profileImgUrl}
+          alt={item.nickname}
         />
         <div className="flex flex-col items-start justify-center">
           <div className="font-medium text-gray-40 text-12">
-            {item.userName} 님의
+            {item.nickname} 님의
           </div>
           <div>{item.islandName} 섬</div>
         </div>
@@ -24,11 +29,7 @@ const SearchResultIslandContainer = ({
       <div className="w-full flex flex-col gap-y-6">
         <div className="w-full flex items-center justify-between text-gray-400 text-12 font-medium">
           <div>오늘의 방문자</div>
-          <div>{item.visit}</div>
-        </div>
-        <div className="w-full flex items-center justify-between text-gray-400 text-12 font-medium">
-          <div>최근 업데이트</div>
-          <div>{item.updated}</div>
+          <div>{item.todayViewCount}</div>
         </div>
       </div>
     </div>
--- src/components/sidebar/search/SearchResultPostContainer.tsx
+++ src/components/sidebar/search/SearchResultPostContainer.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
-import { postListItemType } from '../../../data/type';
+import { mainListPostItemType, postListItemType } from '../../../data/type';
+import { useSearchResultPost } from '../../../hooks/sidebar/search/useSearchResultPost';
 
-const SearchResultPostContainer = ({ item }: { item: postListItemType }) => {
+const SearchResultPostContainer = ({
+  item,
+  moldevId,
+  userName,
+  profileImage,
+}: {
+  item: postListItemType | mainListPostItemType;
+  moldevId: string;
+  userName?: string;
+  profileImage?: string;
+}) => {
+  const { post, userInfo, onClick } = useSearchResultPost(
+    item,
+    moldevId,
+    userName,
+    profileImage,
+  );
   return (
-    <div className="w-full flex flex-col rounded-block bg-white hover:bg-gray-100/40">
+    <div
+      className="w-full flex flex-col rounded-block bg-white hover:bg-gray-100/40 cursor-pointer"
+      onClick={onClick}
+    >
       <div className="w-full flex items-start justify-between p-8 gap-x-12">
         <div className="grow flex flex-col gap-y-6 items-start justify-center">
-          <div className="font-bold">{item.postInfo.title}</div>
-          <div className="text-14 line-clamp-3">{item.postInfo.content}</div>
+          <div className="font-bold">{post.title}</div>
+          <div className="text-14 line-clamp-3">{post.content}</div>
         </div>
         <img
-          className="w-[98px] h-[98px] rounded-block"
-          src={item.postInfo.thumbnail}
-          alt={item.postInfo.title}
+          className="w-[98px] h-[98px] rounded-block object-cover"
+          src={post.thumbnail}
+          alt={post.title}
         />
       </div>
       <div className="w-full flex items-center justify-start p-8 gap-8 text-12">
         <img
           className="w-[20px] h-[20px] rounded-full"
-          src={item.userInfo.profileImgUrl}
-          alt={item.userInfo.nickname}
+          src={userInfo.profileImgUrl}
+          alt={userInfo.nickname}
         />
-        <div className="grow font-medium text-black">
-          {item.userInfo.nickname}
-        </div>
-        <div className="text-gray-300">{item.postInfo.viewCount}</div>
+        <div className="grow font-medium text-black">{userInfo.nickname}</div>
+        <div className="text-gray-300">{post.viewCount}</div>
       </div>
     </div>
   );
