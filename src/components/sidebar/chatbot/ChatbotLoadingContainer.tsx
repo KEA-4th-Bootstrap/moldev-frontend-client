52c2b51e28398e4fae54d1ce6532b39e91f50d3f
Ginieee <kaj1226@naver.com>
2024-06-03 21:50:48 +0900

    💄 [Fix] 챗봇 로딩 컨테이너 레이아웃 수정

--- src/components/sidebar/chatbot/ChatbotLoadingContainer.tsx
+++ src/components/sidebar/chatbot/ChatbotLoadingContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChatbotLoadingContainer = () => {
   return (
-    <div className="px-16 py-10 rounded-rectButton rounded-tl-none bg-gray-50 flex items-center justify-start gap-x-1 ">
+    <div className="px-16 py-10 rounded-rectButton rounded-tl-none bg-gray-50 flex items-center justify-start gap-x-1 w-[70px]">
       <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
       <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
       <div className="h-2 w-2 bg-white rounded-full animate-bounce"></div>
