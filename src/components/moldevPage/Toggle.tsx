5bbcf4cddbf598176d8f0dda43b04d299df0c897
Ginieee <kaj1226@naver.com>
2024-06-07 02:10:52 +0900

    💄 [Feat] Toggle 컴포넌트 디자인 수정

--- src/components/moldevPage/Toggle.tsx
+++ src/components/moldevPage/Toggle.tsx
@@ -10,7 +10,7 @@ const Toggle = ({
 }: {
   type: 'default' | 'main';
   icon: ReactNode;
-  text: string;
+  text?: string;
   isItemHover: boolean;
   onClick: () => void;
   canClick: boolean;
@@ -19,17 +19,19 @@ const Toggle = ({
 
   return (
     <div
-      className={`flex flex-nowrap items-center justify-start gap-x-10 pl-11 pr-14 py-9 rounded-full ${type === 'default' ? 'bg-dark-300/70' : 'bg-main/20'} ${canClick ? 'cursor-pointer' : 'cursor-default'} overflow-hidden h-[42px]  ${isHover || isItemHover ? 'animate-grow' : 'w-[46px]'} transition-all duration-100 ease-in-out`}
+      className={`shrink-0 flex flex-nowrap items-center justify-start gap-x-10 px-11 py-9 rounded-full ${type === 'default' ? 'bg-dark-300/70' : 'bg-main/20'} ${canClick ? 'cursor-pointer' : 'cursor-default'} overflow-hidden h-[42px]  ${isHover || isItemHover ? 'animate-grow' : 'w-[46px]'} transition-all duration-100 ease-in-out`}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
       onClick={onClick}
     >
       <div className="shrink-0 w-[24px] h-[24px]">{icon}</div>
-      <div
-        className={`shrink-0 text-16 font-medium ${type === 'default' ? 'text-white' : 'text-main'}`}
-      >
-        {text}
-      </div>
+      {text && (
+        <div
+          className={`shrink-0 text-16 pr-[3px] font-medium ${type === 'default' ? 'text-white' : 'text-main'}`}
+        >
+          {text}
+        </div>
+      )}
     </div>
   );
 };
